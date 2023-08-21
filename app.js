5bb90b73b773c448816633080b386acfc7b2e6e0
manavptl <manavpatel14072002@gmail.com>
2023-08-21 11:31:47 +0530

    Node.Js project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(process.env.PORT, () => {
+    console.log('server is listening on port 3000');
+});
