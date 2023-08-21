86b54a8f8ca6262999cfae1f2f3d370a407d6ccf
manavptl <manavpatel14072002@gmail.com>
2023-08-21 13:55:17 +0530

    Added Up and Down voting for question and Answer

--- src/controllers/votingController.js
+++ src/controllers/votingController.js
@@ -2,77 +2,115 @@ const BaseController = require('./baseController');
 const Forbidden = require('../errors/Forbidden');
 const NotFound = require('../errors/NotFound');
 const questionModel = require('../models/questionModel');
+const answerModel = require('../models/answerModel'); // Import the Answer model
+const UserModel = require('../models/userModel'); // Import the User model
 const { default: mongoose } = require('mongoose');
 
 module.exports = class Basic extends BaseController {
     async upVoteQuestion(req, res) {
         try {
+            const token = req.userData;
+            const questionId = req.params.questionId;
+
+            const question = await questionModel.findById(questionId);
+
+            if (!question) {
+                throw new NotFound('Question not found');
+            }
+
+            question.upvotes.push(token.userId);
+            await question.save();
+
             return this.sendJSONResponse(
                 res,
-                "Up Voted Question Succesfully",
+                "Up Voted Question Successfully",
                 {
                     length: 1
                 },
-                // user
+                question
             );
         } catch (error) {
-            if (error instanceof NotFound) {
-                console.log(error); // throw error;
-            }
             return this.sendErrorResponse(req, res, error);
         }
     }
 
     async downVoteQuestion(req, res) {
         try {
+            const token = req.userData;
+            const questionId = req.params.questionId;
+
+            const question = await questionModel.findById(questionId);
+
+            if (!question) {
+                throw new NotFound('Question not found');
+            }
+
+            question.downvotes.push(token.userId);
+            await question.save();
+
             return this.sendJSONResponse(
                 res,
-                "Down Voted Question Succesfully",
+                "Down Voted Question Successfully",
                 {
                     length: 1
                 },
-                // user
+                question
             );
         } catch (error) {
-            if (error instanceof NotFound) {
-                console.log(error); // throw error;
-            }
             return this.sendErrorResponse(req, res, error);
         }
     }
 
     async upVoteAnswer(req, res) {
         try {
+            const token = req.userData;
+            const answerId = req.params.answerId;
+
+            const answer = await answerModel.findById(answerId);
+
+            if (!answer) {
+                throw new NotFound('Answer not found');
+            }
+
+            answer.upvotes.push(token.userId);
+            await answer.save();
+
             return this.sendJSONResponse(
                 res,
-                "Up Voted Answer Succesfully",
+                "Up Voted Answer Successfully",
                 {
                     length: 1
                 },
-                // user
+                answer
             );
         } catch (error) {
-            if (error instanceof NotFound) {
-                console.log(error); // throw error;
-            }
             return this.sendErrorResponse(req, res, error);
         }
     }
 
     async downVoteAnswer(req, res) {
         try {
+            const token = req.userData;
+            const answerId = req.params.answerId;
+
+            const answer = await answerModel.findById(answerId);
+
+            if (!answer) {
+                throw new NotFound('Answer not found');
+            }
+
+            answer.downvotes.push(token.userId);
+            await answer.save();
+
             return this.sendJSONResponse(
                 res,
-                "Down Voted Question Succesfully",
+                "Down Voted Answer Successfully",
                 {
                     length: 1
                 },
-                // user
+                answer
             );
         } catch (error) {
-            if (error instanceof NotFound) {
-                console.log(error); // throw error;
-            }
             return this.sendErrorResponse(req, res, error);
         }
     }
